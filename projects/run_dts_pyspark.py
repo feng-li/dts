6d0e977768ac7b10e3bfd4243696a3e34a6d1f73
Feng Li <m@feng.li>
2023-04-19 18:13:40 +0800

    Update config

--- projects/run_dts_pyspark.py
+++ projects/run_dts_pyspark.py
@@ -11,16 +11,16 @@ if hasattr(sys, 'ps1'):
     sys.path.append(libdir)
     logging.info("Interactive session: dts module imported in " + str(libdir))
 
+    ## Pyspark setup
+    import pyspark
+    conf = pyspark.SparkConf().setAppName("Spark DTS App").setAll([
+        # ('spark.ui.enabled', 'false'),
+        ('spark.executor.memory', '128g'),
+        ('spark.cores.max', '64'),
+        ('spark.driver.memory', '64g')
+    ])
+    spark = pyspark.sql.SparkSession.builder.config(conf=conf).getOrCreate()
 
-## Pyspark setup
-import pyspark
-conf = pyspark.SparkConf().setAppName("Spark DTS App").setAll(
-    [('spark.ui.enabled', 'false'),
-     ('spark.executor.memory', '16g'),
-     ('spark.executor.cores', '1'),
-     ('spark.cores.max', '64'),
-     ('spark.driver.memory', '64g')])
-spark = pyspark.sql.SparkSession.builder.config(conf=conf).getOrCreate()
 spark.sparkContext.setLogLevel("WARN")  # "DEBUG", "ERROR"
 
 # Enable Arrow-based columnar data transfers. Ensure that PyArrow is installed and
@@ -63,7 +63,10 @@ conf_mcmc = {
 # Add partition id
 sdf = insert_group_id(sdf=sdf, n_groups=conf_mcmc["n_groups"], method="ts")
 
+pdf = sdf.limit(1000).toPandas()
+
 # One dimensional FFT and periodogram FIXME: This should be done within Spark. The current method should only be used if the resulting NumPy ndarray is expected to be small.
+import numpy as np
 def fft_periodogram(array):  # Construct Periodogram
     """Make an one-dimensional FFT and obtain the periodogram
     """
@@ -74,6 +77,8 @@ def fft_periodogram(array):  # Construct Periodogram
 
 # I_pg_full = fft_periodogram(sdf.select("_c0").to_Pandas().to_numpy())
 I_pg_full = fft_periodogram(np.loadtxt(data_path))
+
+
 
 schema_mapper =
 
